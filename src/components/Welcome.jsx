f9a1d9b7bed21fc1dcc7bf8432cb92e962296d44
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-04-25 20:20:41 -0300

    State sendo ativado com o clique do botão, mas sem mudar o estado do jogo ainda

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,16 +6,14 @@ import Quiz from '../img/quiz.svg';
 import './Welcome.css'
 
 export const Welcome = () => {
-  const quizState = useContext(QuizContext);
-
-  console.log(quizState)
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
     <div id="welcome">
-        <h1>Seja bem-vindo</h1>
-        <p>Clique no botão abaixo para começar:</p>
-        <button>Iniciar</button>
-        <img src={Quiz} alt="Início do Quiz" />
+      <h1>Seja bem-vindo</h1>
+      <p>Clique no botão abaixo para começar:</p>
+      <button onClick={() => dispatch({ type: "CHANGE_STATE" })}>Iniciar</button>
+      <img src={Quiz} alt="Início do Quiz" />
     </div>
   );
 };
