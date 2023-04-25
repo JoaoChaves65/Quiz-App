c6b4a910214bc21626063f6a72a536f645bb4340
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-04-25 12:14:32 -0300

    Usando context na aplicação

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,15 @@
+import { useContext } from 'react';
+import { QuizContext } from '../context/quiz';
+
 import Quiz from '../img/quiz.svg';
 
 import './Welcome.css'
 
 export const Welcome = () => {
+  const quizState = useContext(QuizContext);
+
+  console.log(quizState)
+
   return (
     <div id="welcome">
         <h1>Seja bem-vindo</h1>
@@ -10,5 +17,5 @@ export const Welcome = () => {
         <button>Iniciar</button>
         <img src={Quiz} alt="Início do Quiz" />
     </div>
-  )
-}
+  );
+};
