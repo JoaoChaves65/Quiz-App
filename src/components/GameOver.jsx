bd69461485468b4a2983d5062087f915b403ffc9
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-04-27 19:53:41 -0300

    Personalizando a o componente GameOver com CSS

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import WellDone from "../img/welldone.svg";
+
+import "./GameOver.css";
 
 export const GameOver = () => {
   return (
-    <div>
-        <h2>Game Over</h2>
+    <div id="gameover">
+        <h2>Fim de Jogo!</h2>
+        <p>Pontuação: X</p>
+        <p>Você acertou y de z perguntas.</p>
+        <img src={WellDone} alt="Fim do Quiz" />
+        <button>Reiniciar</button>
     </div>
   )
 }
